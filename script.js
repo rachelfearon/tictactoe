fc2a7dc91da80447dfb798d3565ec07b7f840e79
Rachel Fearon <rachel.fearon@gmail.com>
2021-08-25 22:06:44 -0400

    HTML board setup, displayMoves setup

--- script.js
+++ script.js
@@ -20,12 +20,37 @@ const gameBoard = (function() {
         console.log(board);
     };
 
+    function displayMoves() {
+        const boardContainer = document.querySelector('.boardcontainer');
+        const p = boardContainer.querySelectorAll('p');
+        console.log(p);
+        
+            for (let i = 0; i < board.length; i++) {
+                p[i].textContent = board[i];
+                }
+                
+            }
+            
+
+    const theP = displayMoves();
+
     return {
         board,
-        printBoard
+        printBoard,
+        displayMoves, 
+        theP
     }
 })(); 
 
+
 //set up module for Display Controller
+
+const displayController = (function() {
+    //get the board array of moves; for each item, 
+    console.log(board)
+
+
+    //change textContent of the selected p/box = value of item
+});
 
 //set up factory for players (and moves?)
