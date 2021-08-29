9fa91a271889c855e7e00f10853098f3d437c861
Rachel Fearon <rachel.fearon@gmail.com>
2021-08-29 16:17:47 -0400

    Set up square click event, place token in gameDisplay

--- script.js
+++ script.js
@@ -15,7 +15,37 @@ const Player = (playerName, inputId) => {
 
 
 //board display module
+const gameDisplay = (function() {
+    const boardContainer = document.querySelector('.boardcontainer');
+    const squareList = boardContainer.querySelectorAll('p');
+    //update DOM with player name info upon receiving call from event listener
+    function displayPlayers(playerObj) {
+        console.log("displaying players", playerObj);
+        if (playerObj.token === "X") {
+            const playerOneSpace = document.querySelector(".player1container");
+            let p = document.createElement("p");
+            p.textContent = `${playerObj.name}`;
+            playerOneSpace.append(p);
+        } else if (playerObj.token === "O") {
+            const playerOneSpace = document.querySelector(".player2container");
+            let p = document.createElement("p");
+            p.textContent = `${playerObj.name}`;
+            playerOneSpace.append(p);
+        } else {
+            console.log("displayPlayers error");
+        }
+        
+    };
+
     //populate grid based on board array contents
+    const renderBoard = function() {
+        const square = squareList;
+        const boardMoves = gameLogic.returnBoard();
+        for (let i = 0; i < boardMoves.length; i++) {
+            square[i].textContent = boardMoves[i];
+        }
+    }
+
     //update box display on click (from controller module), receive token input & update textContent
     //display player names/tokens from inputs (from controller on btn submit)
     //display win or tie msg at game end
@@ -23,7 +53,45 @@ const Player = (playerName, inputId) => {
     //display button for reset game
 
 
+    return {
+        displayPlayers,
+        renderBoard,
+        boardContainer,
+        squareList
+    }
+})();
+    
+
+
 //game logic
+const gameLogic = (function() {
+    
+
+    let board = [
+        ``,
+        ``,
+        ``,
+        ``,
+        ``,
+        ``,
+        ``,
+        ``,
+        ``
+    ];
+
+    function returnBoard() {
+        return board;
+    }
+
+    function updateBoard(id, token) {
+        board[id] = token;
+    }
+
+    return {
+        returnBoard,
+        updateBoard
+    }
+})();
     //decide what happens if filled square is clicked
     //decide what happens if empty square is clicked - update whose turn it is after move is made
     //check whose turn it is - or rather, say what happens depending on the turn
@@ -31,9 +99,7 @@ const Player = (playerName, inputId) => {
 
 
 //game controller
-    
 const gameController = (function() {
-    // let playerOne = {};
 
     //const board = gameBoard.boardContainer;
     //On player info submit, pass input value & token to Player FF - receive and store player objs
@@ -41,21 +107,15 @@ const gameController = (function() {
     submitButton.addEventListener('click', event => {
         const name1 = document.getElementById("player1").value;
         const name2 = document.getElementById("player2").value;
-        playerOne = createPlayer(`${name1}`, "player1");
-        playerTwo = createPlayer(`${name2}`, "player2");
-        // playerOne = Player(`${name1}`, `player1`);
-        // playerTwo = Player(`${name2}`, `player2`);
+        playerOne = Player(`${name1}`, `player1`);
+        playerTwo = Player(`${name2}`, `player2`);
         console.log(playerOne);
         console.log(playerTwo);
-        return {playerOne: playerOne};
+        // send it to display function to update DOM
+        gameDisplay.displayPlayers(playerOne);
+        gameDisplay.displayPlayers(playerTwo);
+
     })
-
-
-    function createPlayer(name, token) {
-        let obj = Player(name, token);
-        return obj;
-    }
-
 
     function logPlayer () {
         console.log(playerOne);
@@ -66,13 +126,19 @@ const gameController = (function() {
     //on reset game btn, reset board array, player info, clear display
     //on grid click, check for empty square
     //check whose turn it is
+
     //fill square with appropriate player token
-    // board.addEventListener('click', function(e) {
-    //     console.log(e.target);
-    //     if (!e.target.id) {
-    //         return;
-    //     } else {
-    //         gameBoard.board[e.target.id] = "player token";
+    gameDisplay.boardContainer.addEventListener('click', function(e) {
+        console.log(e.target);
+
+        if (!e.target.id) {
+            return;
+        } else {
+            //TODO replace "X" with selected token var
+            gameLogic.updateBoard(e.target.id, "X");
+            gameDisplay.renderBoard();
+        }
+    })
     //update board array with new player token spot
     //check for winning pattern
     //declare winner, reset
@@ -80,8 +146,7 @@ const gameController = (function() {
     return {
         logPlayer, 
         returnPlayer: function() {return {playerOne, playerTwo}}
-        //playerOne,
-       //playerTwo
+
     }
     
 })();
@@ -91,37 +156,22 @@ const gameController = (function() {
 
 
 
-const gameBoard = (function() {
-    'use strict';
-    const boardContainer = document.querySelector('.boardcontainer');
-    const squareList = boardContainer.querySelectorAll('p');
+// const gameBoard = (function() {
+    
 
-    let board = [
-        `X`,
-        ``,
-        ``,
-        ``,
-        `O`,
-        ``,
-        ``,
-        ``,
-        ``
-    ];
-
-    const renderBoard = (function() {
-        const square = squareList;
-        const boardMoves = board;
-        for (let i = 0; i < board.length; i++) {
-            square[i].textContent = boardMoves[i];
-            }
-    })();
+//     const renderBoard = (function() {
+//         const square = gameDisplay.squareList;
+//         const boardMoves = gameLogic.returnBoard();
+//         for (let i = 0; i < boardMoves.length; i++) {
+//             square[i].textContent = boardMoves[i];
+//             }
+//     })();
 
 
-    return {
-        board, // this will need to be made private, create a function which will update this and export that
-        renderBoard,
-        boardContainer
-    }
-})(); 
+//     return {
+//         renderBoard,
+//         boardContainer
+//     }
+// })(); 
 
 
